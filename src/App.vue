06c00354bb516cc1bd68591fda9556ab206183d3
thetpaingoo <minnthetpaingoo@gmail.com>
2024-04-17 12:51:41 +0630

    First commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<h1>How fast can you catch me?</h1>
+<button @click="clickHandlar" :disabled="isPalying==true">Start</button>
+<div v-if="isPalying">
+  <Block :delay="delay" @stopGame="stopGame"></Block>
+</div>
+
+  <div v-if="end==true">
+  <Result :score="score"/>
+
+</div>
+</template>
+
+
+<script>
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
+export default {
+  name: 'App',
+  components: {
+    Block,Result
+  },
+  data(){
+    return{
+          isPalying : false,
+          delay:null,
+          score:0,
+          end:false
+    }
+  },
+  methods:{
+    clickHandlar(){
+          this.isPalying=true;
+          this.delay=2000+Math.random()*5000;
+          this.end=false;
+    },
+    stopGame(score){
+          this.score=score;
+          this.isPalying=false;
+          this.end=true;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block"  v-if="showblock" @click="stopTimer">click here{{delay}}</div>
+</template>
+
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showblock:false,
+            score:0,
+            timer:null
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this.showblock=true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods:{
+        startTimer(){
+            this.timer=setInterval(() => {
+                this.score+=50;
+            }, 50);
+       
+        },
+        stopTimer(){
+            clearInterval(this.timer);
+            this.$emit("stopGame",this.score)
+        }
+    }
+ 
+    
+}
+</script>
+
+<style>
+.block{
+    background-color: aqua;
+    width:400px;
+    height:300px;
+    margin:20px auto;
+    padding-top:30px;
+}
+</style>
